6503eeda3d2d3a1e76bae3bdf1df01b4a0e975d7
matheus.cavalcanti <matheus.cavalcanti@lavid.ufpb.br>
2025-03-24 10:05:01 -0300

    updates

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,17 +1,19 @@
-// tests/login.spec.js
-const { test } = require('@playwright/test');
+require('dotenv').config();
+const { test, expect } = require('@playwright/test');
 
 test('automate login', async ({ page }) => {
     await page.goto('https://app.tangerino.com.br/Tangerino/?wicket:interface=:6:body:loginForm:baterPontoFuncionario::ILinkListener::');
 
     await page.getByRole('link', { name: 'Registrar Ponto' }).click();
 
-    await page.getByRole('textbox', { name: 'Código do Empregador *' }).fill('WWWW');
+    await page.getByRole('textbox', { name: 'Código do Empregador *' })
+        .fill(process.env.EMPLOYER_CODE);
 
-    await page.getByRole('textbox', { name: 'PIN *' }).fill('1234');
+    await page.getByRole('textbox', { name: 'PIN *' })
+        .fill(process.env.PIN);
 
     await page.getByRole('button', { name: 'Registrar' }).click();
 
     await expect(page.locator('#toast-container')).toBeVisible({ timeout: 5000 });
-
+    await expect(page.getByText('Matheus Dantas Cavalcanti')).toBeVisible();
 });
