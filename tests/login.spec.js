bc075fdc56228f9ac240f37ab9f6928d89e48930
Matheus Dantas Cavalcanti <113441384+matheusdantascavalcanti@users.noreply.github.com>
2026-01-01 08:05:20 -0300

    Atualizar o login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,7 +9,7 @@ test('automate login', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Código do Empregador *' })
         .fill(process.env.EMPLOYER_CODE);
 
-    await page.getByRole('textbox', { name: 'PIN *' })*/
+    await page.getByRole('textbox', { name: 'PIN *' })
         .fill(process.env.PIN);
 
     await page.getByRole('button', { name: 'Registrar' }).click();
@@ -17,3 +17,4 @@ test('automate login', async ({ page }) => {
     await expect(page.locator('#toast-container')).toBeVisible({ timeout: 5000 });
     await expect(page.getByText('Matheus Dantas Cavalcanti')).toBeVisible();
 });
+*/
